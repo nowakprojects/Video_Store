7990e4348806d88d6c55febcc5afb94c83ae3765
Mateusz Nowak <nmateusz96@gmail.com>
2017-04-27 16:29:41 +0200

    Add data in sql

---
+++ src/main/java/pl/nowakprojects/buisnesslogic/implementation/CustomersServiceImpl.java
@@ -0,0 +1,43 @@
+package pl.nowakprojects.buisnesslogic.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import pl.nowakprojects.buisnesslogic.exceptions.CustomerNotFoundException;
+import pl.nowakprojects.buisnesslogic.interfaces.CustomersService;
+import pl.nowakprojects.database.entity.Customer;
+import pl.nowakprojects.database.repository.CustomersRepository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by Mateusz on 22.04.2017.
+ */
+@Service
+public class CustomersServiceImpl implements CustomersService {
+
+    private final CustomersRepository customersRepository;
+
+    @Autowired
+    public CustomersServiceImpl(CustomersRepository customersRepository) {
+        this.customersRepository = customersRepository;
+    }
+
+    public List<Customer> findAll() {
+        return customersRepository.findAll();
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public Customer save(Customer customer) {
+        return customersRepository.save(customer);
+    }
+
+    @Override
+    public Optional<Customer> findOne(Long id) {
+        return id == null ? Optional.empty() : Optional.ofNullable(customersRepository.findOne(id));
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO customer (surname,name,email,phone,address,city)
+VALUES
+    ('Nowak', 'Mateusz', 'mnowak@gmail.com','34232323','Ulica1 2/3','Wrocław'),
+    ('Kowalski','Jan','jan.kowalski@gmail.com','234123123','Ulica3 4/1', 'Warszawa'),
+    ('Kowalski','Jan','koval12@wp.pl','233223123','Ulica3 4/2','Warszawa'),
+    ('Nowak','Anna','nowakowa@gmail.com','233223113','Ulica1 2/2','Wrocław'),
+    ('Wójcik','Władysław','valdi@onet.pl','777777777','Ulica5 2/1', 'Bydgoszcz'),
+    ('Kowalczyk','Przemysław','przemo23@gmail.com','231542123','Ulica2 2/1','Wrocław');
