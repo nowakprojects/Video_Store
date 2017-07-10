80b6c7ccadc4352ee57f8bc9860ae0b5f5a9fd82
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2017-07-10 19:13:09 +0200

    Change db to h2

--- src/main/java/pl/nowakprojects/VideoStoreApplication.java
+++ src/main/java/pl/nowakprojects/VideoStoreApplication.java
@@ -1,7 +1,10 @@
 package pl.nowakprojects;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -12,4 +15,12 @@ public class VideoStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VideoStoreApplication.class, args);
 	}
+
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
 }
